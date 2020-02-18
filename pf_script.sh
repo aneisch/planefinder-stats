10e5c7575e14e1fa95ac780c53ec01dd1f287ae3
Andrew <andrewneisch@gmail.com>
2020-02-18 00:02:06 -0600

    Update pf_script.sh

--- pf_script.sh
+++ pf_script.sh
@@ -4,11 +4,13 @@
 # Uses /ajax/stats endpoint on planefinder and pulls out relevant data from JSON
 
 while true; do 
-  content=$(curl --silent http://$PLANEFINDER_HOST:$PLANEFINDER_PORT/ajax/stats); 
-  mode_s_pps=$(echo $content | jq -r .total_modes_packets_ps); 
-  mode_ac_pps=$(echo $content | jq -r .total_modeac_packets_ps); 
+  content=$(curl --silent http://$PLANEFINDER_HOST:$PLANEFINDER_PORT/ajax/stats)
+  mode_s_pps=$(echo $content | jq -r .total_modes_packets_ps)
+  mode_ac_pps=$(echo $content | jq -r .total_modeac_packets_ps)
   
   curl --silent -XPOST "http://$INFLUX_HOST:$INFLUX_PORT/write?db=$INFLUX_DATABASE" --data-binary "mode_s_pps value=$mode_s_pps
-  mode_ac_pps value=$mode_ac_pps"; 
-  sleep 1; 
+  mode_ac_pps value=$mode_ac_pps"
+  
+  sleep 1
+  
 done
