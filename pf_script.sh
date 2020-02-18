0712dcfea3f4a2e03cb0e1087607e2727bd23efa
Andrew <andrewneisch@gmail.com>
2020-02-17 23:59:59 -0600

    Update pf_script.sh

--- pf_script.sh
+++ pf_script.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Gather data from planefinder client and post Mode S PPS and Mode AC PPS to influxdb. 
+# Uses /ajax/stats endpoint on planefinder and pulls out relevant data from JSON
+
 while true; do 
   content=$(curl --silent http://$PLANEFINDER_HOST:$PLANEFINDER_PORT/ajax/stats); 
   mode_s_pps=$(echo $content | jq -r .total_modes_packets_ps); 
   mode_ac_pps=$(echo $content | jq -r .total_modeac_packets_ps); 
   
-  curl -XPOST "http://$INFLUX_HOST:$INFLUX_PORT/write?db=$INFLUX_DATABASE" --data-binary "mode_s_pps value=$mode_s_pps
-  mode_ac_pps value=$mode_ac_pps"; sleep 1; done
+  curl --silent -XPOST "http://$INFLUX_HOST:$INFLUX_PORT/write?db=$INFLUX_DATABASE" --data-binary "mode_s_pps value=$mode_s_pps
+  mode_ac_pps value=$mode_ac_pps"; 
+  sleep 1; 
+  done
